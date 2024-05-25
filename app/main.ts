a9da6123e209bec3fe41cbf2d7bccf0b5fc8266a
sharmachait <chait8126@gmail.com>
2024-05-25 21:03:47 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,24 +1,35 @@
 import * as fs from 'fs';
-
+import * as zlib from 'zlib';
 const args = process.argv.slice(2);
 const command = args[0];
 
 enum Commands {
     Init = "init",
+    CatFile="cat-file"
 }
 
 switch (command) {
     case Commands.Init:
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         console.log("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        fs.mkdirSync(".git", { recursive: true });
-        fs.mkdirSync(".git/objects", { recursive: true });
-        fs.mkdirSync(".git/refs", { recursive: true });
-        fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
-        console.log("Initialized git directory!");
+        init();
+        break;
+    case Commands.CatFile:
+        if(args.length < 3){
+            throw new Error(`Invalid Options and parameters`)
+        }
+        const option = args[1];
+        const hash=args[2];
+        console.log({option});
+        console.log({hash});
         break;
     default:
         throw new Error(`Unknown command ${command}`);
+}
+
+function init(){
+    fs.mkdirSync(".git", { recursive: true });
+    fs.mkdirSync(".git/objects", { recursive: true });
+    fs.mkdirSync(".git/refs", { recursive: true });
+    fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
+    console.log("Initialized git directory!");
 }
