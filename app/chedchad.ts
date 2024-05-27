a2c5281b43142d669adce009521e7beeff5a8841
sharmachait <chait8126@gmail.com>
2024-05-27 22:54:35 +0530

    stage 5

---
+++ app/chedchad.ts
@@ -0,0 +1,17 @@
+const buffer = Buffer.from('100644 .gitattributes');
+
+// Convert buffer to string to inspect contents (optional, for debugging)
+console.log(buffer.toString()); // Should print '100644 .gitattributes'
+
+// Find the index of the first space character
+const spaceIndex = buffer.indexOf(' ');
+
+if (spaceIndex !== -1) {
+  // Get the sub-buffer from the start to the index of the space character
+  const subBuffer = buffer.slice(0, spaceIndex);
+
+  // Convert the sub-buffer to a string to verify
+  console.log(subBuffer.toString()); // Output: '100644'
+} else {
+  console.log('Space character not found');
+}
--- app/utils/helperFunctions.ts
+++ app/utils/helperFunctions.ts
@@ -24,7 +24,20 @@ export function getContentFromHash(hash: string): Buffer {
   }
 }
 
-export function parseTreeContent(content: string): string[] {
-  console.log(content);
-  return [''];
+export function parseTreeContent(content: Buffer): string[] {
+  let names: string[] = [];
+  let indexOfNull = content.indexOf('\0');
+  let body = content.subarray(indexOfNull + 1, content.length);
+
+  let bodyString = body.toString();
+  let spaceIndex = bodyString.indexOf(' ');
+  while (spaceIndex !== -1) {
+    bodyString = bodyString.substring(spaceIndex + 1, bodyString.length);
+    let nullIndex = bodyString.indexOf('\u0000');
+    let name = bodyString.substring(0, nullIndex);
+    spaceIndex = bodyString.indexOf(' ');
+    names.push(name);
+  }
+  // console.log({ names });
+  return names;
 }
