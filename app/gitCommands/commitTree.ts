76eb91aa6e4189d80ea39c67301a68996893b5ee
sharmachait <chait8126@gmail.com>
2024-06-01 11:54:03 +0530

    commit tree

--- app/gitCommands/commitTree.ts
+++ app/gitCommands/commitTree.ts
@@ -1,5 +1,8 @@
 import { writeBufferToObject } from '../utils/helperFunctions';
-
+import zlib from 'zlib';
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
 export function commitTree(args: string[]): string {
   try {
     const treeSha = args[1];
@@ -18,42 +21,68 @@ export function commitTree(args: string[]): string {
       throw new Error('Invalid arguments');
     }
 
-    const tree = 'tree ' + treeSha;
-    const parent = 'parent ' + parentSha;
+    const tree = `tree ${treeSha}\n`;
+    const parent = `parent ${parentSha}\n`;
     const authorName = process.env.GIT_AUTHOR_NAME || 'Chaitanya Sharma';
     const authorEmail = process.env.GIT_AUTHOR_EMAIL || 'Chai8126@gmail.com';
-    const timeStamp = Math.floor(Date.now() / 1000).toString();
-    const author = 'author ' + authorName + ' ' + authorEmail + ' ' + timeStamp;
+    const timeStamp = Math.floor(Date.now() / 1000);
+    const date = new Date();
+    const timezone = date.getTimezoneOffset();
+    const author =
+      'author ' +
+      authorName +
+      ' <' +
+      authorEmail +
+      '> ' +
+      timeStamp +
+      ' ' +
+      timezone +
+      '\n';
+
     const committer =
-      'committer ' + authorName + ' ' + authorEmail + ' ' + timeStamp;
+      'committer ' +
+      authorName +
+      ' <' +
+      authorEmail +
+      '> ' +
+      timeStamp +
+      ' ' +
+      timezone +
+      '\n\n';
 
-    const nullBuffer = Buffer.from([0]);
-    const newLineBuffer = Buffer.from('\n');
-    const treeBuffer = Buffer.from(tree);
-    const parentBuffer = Buffer.from(parent);
-    const authorBuffer = Buffer.from(author);
-    const committerBuffer = Buffer.from(committer);
-    const messageBuffer = Buffer.from(message);
-    let contentBuffer = Buffer.alloc(0);
-    contentBuffer = Buffer.concat([
-      contentBuffer,
-      treeBuffer,
-      newLineBuffer,
-      parentBuffer,
-      newLineBuffer,
-      authorBuffer,
-      newLineBuffer,
-      committerBuffer,
-      newLineBuffer,
-      messageBuffer,
-      newLineBuffer,
+    let contentBuffer = Buffer.concat([
+      Buffer.from(tree),
+      Buffer.from(parent),
+      Buffer.from(author),
+      Buffer.from(committer),
+      Buffer.from(message),
     ]);
-    const size = contentBuffer.length;
-    const header = `commit ${size}`;
-    const headerBuffer = Buffer.from(header);
-    contentBuffer = Buffer.concat([headerBuffer, nullBuffer, contentBuffer]);
 
-    return writeBufferToObject(contentBuffer);
+    const header = `commit ${contentBuffer.length}\0`;
+
+    contentBuffer = Buffer.concat([Buffer.from(header), contentBuffer]);
+
+    const hash = crypto
+      .createHash('sha1')
+      .update(contentBuffer)
+      .digest('hex')
+      .toString();
+    fs.mkdirSync(
+      path.join(process.cwd(), '.git', 'objects', hash.slice(0, 2)),
+      { recursive: true }
+    );
+    fs.writeFileSync(
+      path.join(
+        process.cwd(),
+        '.git',
+        'objects',
+        hash.slice(0, 2),
+        hash.slice(2)
+      ),
+      zlib.deflateSync(contentBuffer)
+    );
+    return hash;
+    // return writeBufferToObject(contentBuffer);
   } catch (e: any) {
     console.log(e);
     return e.message;
