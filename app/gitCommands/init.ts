3c2eec9aa308dcd4cf27dbfd991b1aeae0b5e112
sharmachait <chait8126@gmail.com>
2024-06-02 11:19:03 +0530

    hostile developer, created directory initialized wiht git

--- app/gitCommands/init.ts
+++ app/gitCommands/init.ts
@@ -7,10 +7,11 @@ export default function init(path: string) {
     fs.mkdirSync('.git/refs', { recursive: true });
     fs.writeFileSync('.git/HEAD', 'ref: refs/heads/main\n');
   } else {
+    console.log({ path });
     fs.mkdirSync(path + '/.git', { recursive: true });
     fs.mkdirSync(path + '/.git/objects', { recursive: true });
     fs.mkdirSync('/.git/refs', { recursive: true });
     fs.writeFileSync(path + '/.git/HEAD', 'ref: refs/heads/main\n');
   }
-  console.log('Initialized git directory!');
+  // console.log('Initialized git directory!');
 }
